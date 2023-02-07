2f0ffcdead00516ad9cd167bd4c8f2d027ed7463
Linvicky0 <117057142+Linvicky0@users.noreply.github.com>
2023-02-07 16:34:24 -0500

    Short description goes here

---
+++ EECS280/p3-euchre/euchre.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  p3-euchre
+//
+//  Created by Vicky Lin on 2/7/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
